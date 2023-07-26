7ddc803e02f41bdd3407f7f5e8e56ce8755f2ee3
Marcus David Alo <marcusdavidalo@gmail.com>
2023-07-27 00:43:42 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center text-center bg-black text-white">
+      <h1 className="text-2xl w-2/3 font-semibold shadow-md shadow-white p-2 rounded-md">
+        Hello and Welcome to the mobile site! This Site is still Currently a
+        Work in Progress!
+      </h1>
     </div>
   );
 }
