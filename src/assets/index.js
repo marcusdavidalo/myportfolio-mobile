59f31cbad2c1bf827399547f870d752be81259bf
Marcus David Alo <marcusdavidalo@gmail.com>
2023-07-27 00:54:56 +0800

    update 5 files and delete 1 file

---
+++ src/assets/index.js
@@ -0,0 +1,50 @@
+// Frontend Skills
+import HTML from "./icons/skills/frontend/HTML.svg";
+import CSSheets from "./icons/skills/frontend/CSS.svg";
+import JS from "./icons/skills/frontend/Javascript.svg";
+import Bootstrap from "./icons/skills/frontend/Bootstrap.svg";
+import TailwindCSS from "./icons/skills/frontend/TailwindCSS.svg";
+import ReactJS from "./icons/skills/frontend/ReactJS.svg";
+import AlpineJS from "./icons/skills/frontend/AlpineJS.svg";
+
+// Backend Skills
+import NodeJS from "./icons/skills/backend/NodeJS.svg";
+import MongoDB from "./icons/skills/backend/MongoDB.svg";
+import ExpressJS from "./icons/skills/backend/ExpressJS.svg";
+// import Mongoose from "./icons/skills/backend/Mongoose.svg";
+
+// Tools
+import Git from "./icons/skills/tools/Git.svg";
+import Github from "./icons/skills/tools/Github.svg";
+import NPM from "./icons/skills/tools/NPM.svg";
+import Postman from "./icons/skills/tools/Postman.svg";
+import MongoDBCompass from "./icons/skills/tools/MongoDBCompass.svg";
+import VSCode from "./icons/skills/tools/VSCode.svg";
+
+const frontendSkills = {
+  HTML,
+  CSSheets,
+  JS,
+  Bootstrap,
+  TailwindCSS,
+  ReactJS,
+  AlpineJS,
+};
+
+const backendSkills = {
+  NodeJS,
+  MongoDB,
+  ExpressJS,
+  //   Mongoose,
+};
+
+const tool = {
+  Git,
+  Github,
+  MongoDBCompass,
+  NPM,
+  Postman,
+  VSCode,
+};
+
+export { frontendSkills, backendSkills, tool };
